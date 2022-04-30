a5f6edfb2e0329042fb972e2ae0295638835a664
Alexander Smirnov <aleqsonder@icloud.com>
2022-04-30 12:19:16 +0300

    Завершил работу над мобильной версией

--- docs/js/dynamic-header.js
+++ docs/js/dynamic-header.js
@@ -1,8 +1,12 @@
 var maxStaticScroll = 0;
 
 $(window).scroll(function() {
-	if ($(window).scrollTop() > maxStaticScroll)
-		$("#header").addClass("active");
-	else
-		$("#header").removeClass("active");
+	if ($(window).scrollTop() > maxStaticScroll){
+		$("#header").addClass("fixed");
+		$("body").addClass("shifted");
+	}
+	else{
+		$("#header").removeClass("fixed");
+		$("body").removeClass("shifted");
+	}
 });
